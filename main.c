1d4722e171ea2d2b4ac5b714ee879daa99ef8b68
uvuvwevwevwe onyetenyevwe ugwemuhwem osas <chandaman1111@gmail.com>
2025-07-14 14:41:22 +0800

    Hot fix, add non-perfect elastic collision. Add .gitignore file.
    Reduce the chance of ball bouncing up and down non-stop at the end.
    Add .gitignore file and remove main.o binary file. It accidently added last commit.

--- main.c
+++ main.c
@@ -11,20 +11,21 @@ double degree_2_radius (double degree)
 
 int main()
 {
-    float gravity = 4.8, air_friction_strength=0.02;
-    int worldSizeX=60, worldSizeY=20;
+    float gravity = 6.8, air_friction_strength=0.02, material_acc_absorb_rate=0.25;
+    int worldSizeX=45, worldSizeY=20;
     int world[worldSizeY][worldSizeX];//MAP
-    double curX=1, curY=1, prevX, prevY, curForceX, curForceY;
+    double curX=1, curY=1, prevX, prevY, curAccX, curAccY;
     double delta_time,sim_time=0;
-    int defaultAngl=60;
-    float defaultForce=3;
+    int fixedX=0,fixedY=0;
+    int defaultAngl=46;
+    float defaultAcc=2;
     struct timeval delta_time_gravity_prev,delta_time_gravity_cur;
     int dummy;
     
     int exceed_map_X_flag=0,exceed_map_Y_flag=0;//if ball go to fast, it pass through the wall, active this flag, wall won't block the ball, deactivate it when the ball return to the map.
 
-    curForceX=cos(degree_2_radius(defaultAngl))*defaultForce;
-    curForceY=sin(degree_2_radius(defaultAngl))*defaultForce;   
+    curAccX=cos(degree_2_radius(defaultAngl))*defaultAcc;
+    curAccY=sin(degree_2_radius(defaultAngl))*defaultAcc;   
 
     //build(print) the world
     system("clear");
@@ -44,38 +45,48 @@ int main()
         delta_time_gravity_prev=delta_time_gravity_cur;
         sim_time+=delta_time;
 
-        //Gravity to force
-        curForceY-=gravity*delta_time;
-        //Air Friction to force
-        curForceX-=curForceX*air_friction_strength;
-        curForceY-=curForceY*air_friction_strength;
-        //Hit to force
+        //Gravity to Acc
+        curAccY-=gravity*delta_time;
+        //Air Friction to Acc
+        curAccX-=curAccX*air_friction_strength;
+        curAccY-=curAccY*air_friction_strength;
+        //The ball back to the map, remove flag
+        if(curX>0 && curX<worldSizeX)
+            exceed_map_X_flag=0;
+        if(curY>0 && curY<worldSizeY)
+            exceed_map_Y_flag=0;
+        //Hit to Acceleration
         if((curX<=0 || curX>=worldSizeX) && !exceed_map_X_flag)
             {
-                curForceX=-curForceX;
+                curAccX=-(curAccX*(1-material_acc_absorb_rate));
                 exceed_map_X_flag=1;
             }
         if((curY<=0 || curY>=worldSizeY) && !exceed_map_Y_flag)
             {
-                curForceY=-curForceY;
+                curAccY=-(curAccY*(1-material_acc_absorb_rate));
                 exceed_map_Y_flag=1;
             }
-        //The ball back to the map, remove flag
-        if(curX>0 || curX<worldSizeX)
-            exceed_map_X_flag=0;
-        if(curY>0 || curY<worldSizeY)
-            exceed_map_Y_flag=0;
-        //Force too low == stop. As force can never be 0 in this programme
-        if(curForceX<0.2 && curForceX>-0.2)
-            curForceX=0;
-        if(curForceY<1 && curForceY>0)
-            curForceY=0;
-        //calculate new position
-        curX=curX+curForceX;
-        curY=curY+curForceY;
+        //Acceleration too low == stop. As Acc can never be 0 in this programme
+        if(curAccX<0.2 && curAccX>-0.2)
+            {
+                curX=round(curX);
+                fixedX=1;
+                curAccX=0;
+            }
+        if(curAccY<0.5 && curAccY>0 && curY<=0)//stop when up acceleration too low
+            {
+                curY=0;
+                fixedY=1;
+                curAccY=0;
+            }
+        //Calculate new position
+        if(!fixedX)
+            curX=curX+curAccX;
+        if(!fixedY)
+            curY=curY+curAccY;
         //print frame
         printf("\033[1;1H");
-        printf("FRAME %d SIMed: %.2fms, [%.2f,%.2f] {%.2f,%.2f}     ",i,sim_time,curX,curY,curForceX,curForceY);
+        printf("FRAME %d SIMed: %.2fms, [%.2f,%.2f] {%.2f,%.2f}, Exceed Map:%d    ",i,sim_time,curX,curY,curAccX,curAccY,exceed_map_X_flag+exceed_map_Y_flag);
         //Remove ball from last frame
         printf("\033[%d;%dH ", worldSizeY - (int)round(prevY), (int)round(prevX) + 1);
         printf(" ");
